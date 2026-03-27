17a8e7a9e88cd7fc6f9446cbb8172ed0c92d2662
Djordje Stojkovic <djole8793@gmail.com>
2026-03-27 15:55:32 -0500

    vezbanje

---
+++ app/Models/Ocene.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use mysql_xdevapi\Table;
+
+class Ocene extends Model
+{
+    protected $table = "ocene";
+
+    protected $fillable = [
+
+    ];
+}
